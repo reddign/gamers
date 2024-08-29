53c421770265368b59aa6a9accce41f354164958
iskeete12 <ianskeete1@gmail.com>
2024-08-29 09:11:14 -0400

    Adding my page to group project

---
+++ web_src/general/about/Ian_Skeete.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Ian Skeete's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Ian Skeete's Bio">
+            <div class="bio-title">Ian Skeete</div>
+            <br>
+            <div id="bio">"I am a senior information systems major with no minors. I will also be graduating this spring of 2025."
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
