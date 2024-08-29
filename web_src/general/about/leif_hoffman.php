684153ae0abe999813a7d5abe85e2afd1db713cc
LeifHoffman <hoffmanl2@etown.edu>
2024-08-29 08:52:13 -0400

    adding my page to group project

---
+++ web_src/general/about/leif_hoffman.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Leif Hoffman's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Leif Hoffman's Bio">
+            <div class="bio-title">Leif Hoffman</div>
+            <br>
+            <div id="bio"> Leif Hoffman is a computer engineering major, with a minor in coaching. His graduation year is 2026.
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
