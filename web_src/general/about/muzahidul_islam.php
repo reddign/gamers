5c81727df448bb45375dae75734d7a3244a4d62a
muzzy3k <mmi15@protonmail.com>
2024-08-29 08:40:44 -0400

    adding my page to group project

--- web_src/general/about/muzahidul_islam.php
+++ web_src/general/about/muzahidul_islam.php
@@ -1,5 +1,5 @@
 <?php
-$pageName = "Jane Doe's Webpage";
+$pageName = "Muzahidul Islam's Webpage";
 require "../../includes/functions.php";
 require "../../includes/head.php";
 
@@ -14,11 +14,10 @@ require "../../includes/head.php";
     </div>
         
     <main>
-        <section id="Jane Doe's Bio">
-            <div class="bio-title">Jane Doe</div>
+        <section id="Muzahidul Islam's Bio">
+            <div class="bio-title">Muzahidul Islam</div>
             <br>
-            <div id="bio"> Jane Doe is not a student in this class. If she was she might be majoring in Computer Science, with a concentration in Web and Application 
-                Design, plus a minor in Graphic Design, and intends to graduate in 2026.
+            <div id="bio"> Muzahidul Islam is a student at Elizabethtown College studying Information Systems and Data Analytics
         </section>
     </main>
 </body>
