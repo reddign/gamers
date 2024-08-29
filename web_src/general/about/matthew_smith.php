79f055cefee6ed9c49a7c81dd6d73b7c618ca187
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-08-29 19:23:06 -0400

    adding my page to group project

---
+++ web_src/general/about/matthew_smith.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Matt Smith's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Matt Smith's Bio">
+            <div class="bio-title">Matt Smith</div>
+            <br>
+            <div id="bio"> Hello, I'm Matt a Computer Science Major and Math Minor! I intend to graduate in 2026.
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
