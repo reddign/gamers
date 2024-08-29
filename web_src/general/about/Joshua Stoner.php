b450406619c96d6cfcd609484afd9aa2b0401a02
STONERJ25 <stonerj@etown.edu>
2024-08-29 13:10:59 -0400

    adding my page to group project

---
+++ web_src/general/about/Joshua Stoner.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Joshua Stoner's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Joshua Stoner's Bio">
+            <div class="bio-title">Joshua Stoner</div>
+            <br>
+            <div id="bio"> Joshua Stoner, Computer Engineering 2025
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
