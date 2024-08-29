44b7413b56059d4c7063310241b7ed7e96e8f39a
Vincent Liu <liuv@etown.edu>
2024-08-29 13:13:56 +0900

    Added personal page to project.

---
+++ web_src/general/about/vincent_liu.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Vincent Liu's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Vincent Liu's Bio">
+            <div class="bio-title">Vincent Liu</div>
+            <br>
+            <div id="bio"> Vincent Liu is a student in this class. Majoring in Computer Engineering with a minor in Japanese and part of the class of 2026
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
