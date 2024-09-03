b4d80759d5b0e6e919d48113fc290890f9bd78bc
joshschmitt35 <157548551+joshschmitt35@users.noreply.github.com>
2024-09-03 09:51:35 -0400

    adding my page to group project

---
+++ web_src/general/about/Josh Schmitt.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Josh Schmitt's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Josh Schmitt's Bio">
+            <div class="bio-title">Josh Schmitt</div>
+            <br>
+            <div id="bio"> I am a engineering major with a concentration in computers and I intend to graduate in May of 2025. 
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
