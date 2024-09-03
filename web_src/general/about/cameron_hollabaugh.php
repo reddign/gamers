7ede84bf60d0b9588e78b45930106e65417a87ca
HollabaughC <143000835+HollabaughC@users.noreply.github.com>
2024-09-03 10:19:06 -0400

    added page

---
+++ web_src/general/about/cameron_hollabaugh.php
@@ -0,0 +1,27 @@
+<?php
+$pageName = "Jane Doe's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Cameron's Bio">
+            <div class="bio-title">Cameron</div>
+            <br>
+            <div id="bio"> Cameron is n̶o̶t̶ a student in this class. He is majoring in Japanese and Information Systems,
+                plus several minors and intends to graduate in 2025.
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
