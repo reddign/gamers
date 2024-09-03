eb19e87f5b8e598dfe94bbfe54d62853b8c2ca91
Martin Ratchford <ratchfordm@etown.edu>
2024-09-03 10:29:01 -0400

    adding my page to group project

---
+++ web_src/general/about/MartinRatchford.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Martin Ratchford Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Martin Ratchford's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio"> Martin Ratchford is a computer science major at Etown and plans to graduate in 2026git.
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
