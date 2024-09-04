b23ed4b682e8f15e8e5679cd9d5e762318c5e5ba
Alex Roop <158513920+Acroop35@users.noreply.github.com>
2024-09-03 21:25:33 -0400

    adding my page to group project

---
+++ web_src/general/about/AlexRoop.php
@@ -0,0 +1,27 @@
+<?php
+$pageName = "Alex Roops's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Alex Roops's Bio">
+            <div class="bio-title">Alex Roop</div>
+            <br>
+            <div id="bio"> Alex Roop is a junior with a major in computer science with a contentration in data science
+                            and a minor in math, and intends to graduate in 2026.
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
