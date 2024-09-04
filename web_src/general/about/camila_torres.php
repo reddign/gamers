ab30ab9c44606161d1f0399df2e1d3dfe8947aca
Professor Reddig <reddign@etown.edu>
2024-09-04 12:54:30 -0400

    Brought back file.

---
+++ web_src/general/about/camila_torres.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Camila Torres's Webpage";
+require "../../includes/functions.php";
+require "../../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Camila Torres's Bio">
+            <div class="bio-title">Camila Torres</div>
+            <br>
+            <div id="bio"> Camila Torres is a student in this class. She is a Data Science major. She intends to graduate 2025.
+        </section>
+    </main>
+</body>
+<?php
+     require "../../includes/footer.php";
+?>
