00f7831ba1badc98aa57b2b0e0b4d06d4e0e001a
Brian Duva <duvab@etown.edu>
2024-09-05 08:30:59 -0400

    Brian Duva face and name hyperlinks added

--- web_src/general/about/SEteam2024.php
+++ web_src/general/about/SEteam2024.php
@@ -29,13 +29,15 @@ require "../../includes/head.php";
                 <area shape="circle" coords="610,139,10" href="James_Hutchins.php">
                 <area shape="circle" coords="157,112,10" href="leif_hoffman.php">
                 <area shape="circle" coords="188,138,10" href="Joshua_Stoner.php">
+                <area shape="circle" coords="700,145,10" href="Brian_Duva.php">
+
             </map>    
         </div>
 
         <div class="section" id="team-names">
             <!-- <span class="name">From Left to Right: </span>  -->
             <span class="name">
-                <a href="kevin_barbieri.php">Kevin Barbieri</a>, Joseph Culkin, Brian Duva, Parker Engle,
+                <a href="kevin_barbieri.php">Kevin Barbieri</a>, Joseph Culkin, <a href="Brian_Duva.php">Brian Duva</a>, Parker Engle,
                 <a href="leif_hoffman.php">Leif Hoffman</a>, Cameron Hollabaugh, Laney Humble, <a href="James_Hutchins.php">James Hutchins<a/> ,
                 <a href='muzahidul_islam.php'>Muzahidul Islam</a>, <a href='vincent_liu.php'>Vincent Liu</a>, <a href='john_mcgovern.php'>John McGovern</a>, Jackson Miller, <a href='Joshua_Schmitt.php'>
                 Mildred Nwachukwu-Innocent, Martin Ratchford, Alexander Roop, <a href='wesJRyan.php'>Wes J. Ryan</a>,
