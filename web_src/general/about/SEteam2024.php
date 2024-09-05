8b3c2da6006a7b8577da8e0b573f07626c3a94d7
HollabaughC <143000835+HollabaughC@users.noreply.github.com>
2024-09-05 08:46:31 -0400

    commit to main

--- web_src/general/about/SEteam2024.php
+++ web_src/general/about/SEteam2024.php
@@ -20,19 +20,26 @@ require "../../includes/head.php";
         <div class="section"> This project was taken on and continued by the Fall 2024 Software Engineering class. <BR></div>
         <br>
         <div class="pfp">
-            <img id="Team2024Pic" src="../teampics/SEteam2024.png" alt="2024 Team to Come picture." width=1000 usemap="#classmap">
+            <img id="Team2024Pic" src="../teampics/SEteam2024_A.png" alt="2024 Team to Come picture." width=1000 usemap="#classmap">
             <map id="classmap">
-                <area shape="circle" coords="258,148,20" href="student_template.php">
+                <area shape="circle" coords="225,83,20" href="student_template.php">
+                <area shape="circle" coords="573,140,10" href="muzahidul_islam.php">
+                <area shape="circle" coords="515,145,10" href="OwenWertzberger.php">
+                <area shape="circle" coords="800,153,10" href="kevin_barbieri.php">
+                <area shape="circle" coords="610,139,10" href="James_Hutchins.php">
+                <area shape="circle" coords="157,112,10" href="leif_hoffman.php">
+                <area shape="circle" coords="490,150,10" href="cameron_hollabaugh.php">
+                
             </map>    
         </div>
 
         <div class="section" id="team-names">
             <!-- <span class="name">From Left to Right: </span>  -->
             <span class="name">
-                Kevin Barbieri, Joseph Culkin, Brian Duva, Parker Engle,
-                Leif Hoffman, Cameron Hollabaugh, Laney Humble, James Hutchins,
+                <a href="kevin_barbieri.php">Kevin Barbieri</a>, Joseph Culkin, Brian Duva, Parker Engle,
+                <a href="leif_hoffman.php">Leif Hoffman</a>, <a href="cameron_hollabaugh.php">Cameron Hollabaugh</a>, Laney Humble, <a href="James_Hutchins.php">James Hutchins<a/> ,
                 <a href='muzahidul_islam.php'>Muzahidul Islam</a>, <a href='vincent_liu.php'>Vincent Liu</a>, John McGovern, Jackson Miller,
-                Mildred Nwachukwu-Innocent, Martin Ratchford, Alexander Roop, Wesley Ryan,
+                Mildred Nwachukwu-Innocent, Martin Ratchford, Alexander Roop, <a href='wesJRyan.php'>Wes J. Ryan</a>,
                 Joshua Schmitt, Ian Skeete, Austin Smith, Matthew Smith,
                 Tyler Souders, Joshua Stoner, Camila Torres, Joey Wagner,
                 Asher Wayde, <a href='OwenWertzberger.php'>Owen Wertzberger</a>, <a href='owen_yang.php'>Owen Yang</a>, and <a href='student_template.php'>Jane Doe</a>
