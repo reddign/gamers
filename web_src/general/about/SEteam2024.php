37e09c2c14fc8c5a47040b33621f333c80d02555
iskeete12 <ianskeete1@gmail.com>
2024-09-06 15:45:53 -0400

    Adding hypertext link

--- web_src/general/about/SEteam2024.php
+++ web_src/general/about/SEteam2024.php
@@ -49,6 +49,8 @@ require "../../includes/head.php";
                 <area shape="circle" coords="55,141,10" href="vincent_liu.php">
                 <area shape="circle" coords="828,130,10" href="Tyler_Souders.php">
                 <area shape="circle" coords="288,138,10" href="owen_yang.php">
+                <area shape="circle" coords="925,154,10" href="Ian_Skeete.php">
+                
             </map>    
         </div>
 
@@ -59,7 +61,7 @@ require "../../includes/head.php";
                 <a href="leif_hoffman.php">Leif Hoffman</a>, <a href="cameron_hollabaugh.php">Cameron Hollabaugh</a>, Laney Humble, <a href="James_Hutchins.php">James Hutchins</a>,
                 <a href='muzahidul_islam.php'>Muzahidul Islam</a>, <a href='vincent_liu.php'>Vincent Liu</a>, <a href="john_mcgovern.php">John McGovern</a>, <a href="Jackson_Miller.php">Jackson Miller</a>,
                 Mildred Nwachukwu-Innocent, <a href='MartinRatchford.php'>Martin Ratchford</a>, <a href='AlexRoop.php'> Alexander Roop </a>, Wesley Ryan,
-                Joshua Schmitt, Ian Skeete, <a href='austin_smith.php'>Austin Smith</a>, <a href='matthew_smith.php'>Matthew Smith,
+                Joshua Schmitt, <a href="Ian_Skeete.php">Ian Skeete</a> , <a href='austin_smith.php'>Austin Smith</a>, <a href='matthew_smith.php'>Matthew Smith,
                 <a href='Tyler_Souders.php'>Tyler Souders</a>, Joshua Stoner, <a href='camila_torres.php'>Camila Torres</a>, <a href='joey_wagner.php'>Joey Wagner</a>,
                 Asher Wayde, <a href='OwenWertzberger.php'>Owen Wertzberger</a>, <a href='owen_yang.php'>Owen Yang</a>, and <a href='student_template.php'>Jane Doe</a>
             </span>
