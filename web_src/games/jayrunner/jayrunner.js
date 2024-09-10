ff847fd5fa17bb9087c084e3b33c75d4544a8c2a
muzzy3k <mmi15@protonmail.com>
2024-09-10 10:47:28 -0400

    organize file structure and add todos

--- web_src/games/jayrunner/jayrunner.js
+++ web_src/games/jayrunner/jayrunner.js
@@ -1,30 +1,95 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const bird = document.getElementById('bird');
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext("2d");
 
-    let gravity = 50;
-    let playerpos = 0;
-    let grounded = true;
-    let vspeed = 0;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    function jump() {
-        if (grounded == true) {
-            vspeed = 500;
-        }
+// load images for player
+const playerWalkImg = new Image();
+playerWalkImg.src = 'images/bluejaywalk.png';
+const playerJumpImg = new Image();
+playerJumpImg.src = 'images/bluejayjump.png';
+
+
+// create a player object so its easier variable organization
+const player = {
+    x: 50,
+    y: canvas.height - 150,
+    width: 50,
+    height: 50,
+    dy: 0,  // change in, can use player.dy to calculate change in y to update playter img
+    gravity: 0.5,
+    grounded: false,
+    currentImg: playerWalkImg
+};
+
+let gravity = 50;
+let playerpos = 0;
+let grounded = true;
+let vspeed = 0;
+let FPS = 0;
+let isGameOver = false;
+
+// Draw the player on the canvas using image
+function drawPlayer() {
+    context.drawImage(player.currentImg, player.x, player.y, player.width, player.height);
+}
+
+function updatePlayer() {
+    //TODO Update player logic and images here
+}
+
+function drawObstacles() {
+    // generate and draw the different obstacles. can use generated sprites later
+}
+
+function updateObstacles() {
+    // TODO: Update Obstacle positioon and logic
+}
+
+function checkCollision() {
+    //  TODO: CHeck PLayer Collision with obstacle
+}
+
+function drawScore() {
+    // TODO: Draw Score
+}
+
+function jump() {
+    if (grounded == true) {
+        vspeed = 500;
     }
+}
 
-    function gameLoop() {
-        if (grounded == false) {
-            vspeed -= gravity;
-        }
 
-        playerpos += vspeed;
+function gameOver() {
+    // TODO: add gameover logic
+}
 
-        if (grounded == true) {
-            vspeed = 0;
-        }
-        bird.style.bottom = playerpos + 'px';
+function gameLoop() {
+    if (!isGameOver) {
+
+        updatePlayer();
+        checkCollision();
+
+        drawPlayer();
+        drawObstacles();
+        drawScore();
     }
+}
 
-    document.addEventListener('keydown', jump);
-    gameLoop();
-});
+// Start the game when images are loaded
+let imagesLoaded = 0;
+function imageLoaded() {
+    imagesLoaded++;
+    // only bird images right now so value is 2, obstacles later
+    if (imagesLoaded === 2) {
+        gameLoop();
+    }
+}
+
+// load images
+playerWalkImg.onload = imageLoaded;
+playerJumpImg.onload = imageLoaded;
+
+document.addEventListener('keydown', jump());
