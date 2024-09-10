51148520370d84e105eb43aae3902bb79e631c29
Laneyeh <humblel@etown.edu>
2024-09-09 20:37:59 -0400

    made changes to main page and added hyperlink

--- web_src/general/about/SEteam2024.php
+++ web_src/general/about/SEteam2024.php
@@ -57,8 +57,8 @@ require "../../includes/head.php";
         <div class="section" id="team-names">
             <!-- <span class="name">From Left to Right: </span>  -->
             <span class="name">
-                Kevin Barbieri, <a href="joseph_culkin.php">Joseph Culkin, <a href="Brian_Duva.php">Brian Duva</a>, <a href="ParkerEngle.php">Parker Engle</a>,
-                <a href="leif_hoffman.php">Leif Hoffman</a>, <a href="cameron_hollabaugh.php">Cameron Hollabaugh</a>, Laney Humble, <a href="James_Hutchins.php">James Hutchins</a>,
+                Kevin Barbieri, <a href="joseph_culkin.php">Joseph Culkin, <a href="Brian_Duva.php">Brian Duva</a>,<a href="ParkerEngle.php">Parker Engle</a>,
+                <a href="leif_hoffman.php">Leif Hoffman</a>, <a href="cameron_hollabaugh.php">Cameron Hollabaugh</a>, <a href="laney_humble.php">Laney Humble</a>, <a href="James_Hutchins.php">James Hutchins</a>,
                 <a href='muzahidul_islam.php'>Muzahidul Islam</a>, <a href='vincent_liu.php'>Vincent Liu</a>, <a href="john_mcgovern.php">John McGovern</a>, <a href="Jackson_Miller.php">Jackson Miller</a>,
                 <a href='MildredNwachukwu.php'>Mildred Nwachukwu-Innocent</a>, <a href='MartinRatchford.php'>Martin Ratchford</a>, <a href='AlexRoop.php'> Alexander Roop </a>, Wesley Ryan,
                 Joshua Schmitt, <a href="Ian_Skeete.php">Ian Skeete</a> , <a href='austin_smith.php'>Austin Smith</a>, <a href='matthew_smith.php'>Matthew Smith,
