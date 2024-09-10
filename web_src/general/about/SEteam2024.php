6a43c7030830ea338ad66bbee1e486a48b1ef2b4
Asher Wayde <Waydea@etown.edu>
2024-09-10 09:58:21 -0400

    Fixing the link i broke

--- web_src/general/about/SEteam2024.php
+++ web_src/general/about/SEteam2024.php
@@ -63,7 +63,7 @@ require "../../includes/head.php";
                 <a href='MildredNwachukwu.php'>Mildred Nwachukwu-Innocent</a>, <a href='MartinRatchford.php'>Martin Ratchford</a>, <a href='AlexRoop.php'> Alexander Roop </a>, Wesley Ryan,
                 Joshua Schmitt, <a href="Ian_Skeete.php">Ian Skeete</a> , <a href='austin_smith.php'>Austin Smith</a>, <a href='matthew_smith.php'>Matthew Smith,
                 <a href='Tyler_Souders.php'>Tyler Souders</a>, Joshua Stoner, <a href='camila_torres.php'>Camila Torres</a>, <a href='joey_wagner.php'>Joey Wagner</a>,
-                <a href='Asher_Wayde.php'>Asher Wayde</a>OwenWertzberger.php'>Owen Wertzberger</a>, <a href='owen_yang.php'>Owen Yang</a>, and <a href='student_template.php'>Jane Doe</a>
+                <a href='Asher_Wayde.php'>Asher Wayde</a>, <a href='OwenWertzberger.php'>Owen Wertzberger</a>, <a href='owen_yang.php'>Owen Yang</a>, and <a href='student_template.php'>Jane Doe</a>
             </span>
         </div>
     </div>
