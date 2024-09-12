2490488030fbc03766068dad79ccf758f870bec2
Brian Duva <duvab@etown.edu>
2024-09-12 08:10:11 -0400

    start of squirrel stub code

---
+++ web_src/games/carrotcake/squirrel.js
@@ -0,0 +1,17 @@
+//code for controlling the squirrel enemies
+//includes animation code as well as code for targetting and pathfinding
+
+class Squirrel {
+
+    //position of squirrel
+    xPos;
+    yPos;
+
+    //target position of squirrel that it will path to
+    xTar;
+    yTar;
+
+    //which targetting method the squirrel will use, will be varied to prevent all squirrels following the same path
+    tarMethod;
+
+}
