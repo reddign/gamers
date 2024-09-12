23dbaccedb8658000e6e2bcf45a82bf800b67f91
Asher Wayde <Waydea@etown.edu>
2024-09-12 09:52:04 -0400

    Adding the word structure to the word.php file

--- data_src/api/wordsearch/word.php
+++ data_src/api/wordsearch/word.php
@@ -1,8 +1,21 @@
 <?php
-// this will contain the word structure for the board
 
 
+class Word{
+    public $name;
+    public $length;
+    public $angle=NULL;
+    public $start=NULL;
+    public $end=NULL;
 
+    function __construct($name)
+    {
+        $this->name = $name;
+        $this->length = strlen($name);
+    }
+
+}
+// this is the basic word structure that goes on the board
 
 
 
