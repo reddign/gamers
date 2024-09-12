f58949d988c0cde90aa6949564cfb6d1ea3010aa
Austin Smith <smitha11@etown.edu>
2024-09-12 10:36:38 -0400

    actions js

---
+++ web_src/games/ESA/unNamed.js
@@ -0,0 +1,17 @@
+class actions{
+    constructor(){
+    this.name= "action"
+    function look(currentLocation){
+
+        }
+    function goto(currentLocation,destination){
+
+
+        }
+    function
+    }
+   
+
+
+
+}
