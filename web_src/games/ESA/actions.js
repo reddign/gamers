7203538c997cd9e35fb33961d5225aea21f2a44b
Austin Smith <smitha11@etown.edu>
2024-09-12 10:44:16 -0400

    updated actions

---
+++ web_src/games/ESA/actions.js
@@ -0,0 +1,13 @@
+class Actions {
+    constructor() {
+        this.name = "actions";
+    }
+
+    look(currentLocation) {
+        // Define what the look function should do here
+    }
+
+    goto(currentLocation, destination) {
+        // Define what the goto function should do here
+    }
+}
