2439170c5153f3d70e3a06778623233407521358
Mildred1999 <nwachukwu.mildred10@gmail.com>
2024-09-12 10:05:45 -0400

    creating functions

---
+++ web_src/games/ESA/Etown.js
@@ -0,0 +1,32 @@
+let score;
+let name;
+let major;
+
+
+function startgame(){
+    // shows a welcome page 
+    // displays start game button
+
+}
+
+function morning_routine(){
+    // describes first scenarion in dorm room
+    // 
+}
+
+function first_class(){
+    // finds first class 
+    // 
+}
+
+function have_eaten(){
+    
+}
+
+function has_keycard(){
+
+}
+
+function first_lecture(){
+    
+}
