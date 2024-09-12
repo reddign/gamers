501e884c82858b69996179076edf2de443e87e81
STONERJ25 <stonerj@etown.edu>
2024-09-12 09:14:21 -0400

    Added Fly

--- web_src/games/JayHunt/jayhunt.js
+++ web_src/games/JayHunt/jayhunt.js
@@ -11,8 +11,12 @@ let jay_1_see = 0;
 let jay_2_see = 0;
 let mouse_x = 0;
 let mouse_y = 0;
-
+let radius = 10;
 let round = 0;
+let  directionx1 = 1;
+let  directiony1 = 1;
+let  directionx2 = 1;
+let  directiony2 = 1;
 
 function roundstart(){
 
@@ -26,7 +30,54 @@ function roundover(){
 }
 
 function fly(){
-    
+    if(jay_1_see){
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.arc(jay_1_x,jay_1_y,radius,0,2*Math.PI);
+    context.closePath();
+    context.fill();}
+    if(jay_2_see){
+        context.fillStyle = "blue";
+        context.beginPath();
+        context.arc(jay_2_x,jay_2_y,radius,0,2*Math.PI);
+        context.closePath();
+        context.fill();        
+    }
+
+}
+
+
+function flybad(){
+
+   
+    if(jay_1_x+radius>canvas.width){
+        directionx1 = -1;
+    }
+    if(jay_1_y+radius>canvas.height){
+        directiony1 = -1;
+    }
+    if(jay_1_x < 0+radius){
+        directionx1 = 1;
+    }
+    if(jay_1_y < 0+radius){
+        directiony1 = 1;
+    }
+    jay_1_x = jay_1_x+2*directionx1;
+    jay_1_y = jay_1_y+2*directiony1;
+    if(jay_2_x+radius>canvas.width){
+        directionx2 = -1;
+    }
+    if(jay_2_y+radius>canvas.height){
+        directiony2 = -1;
+    }
+    if(jay_2_x < 0+radius){
+        directionx2 = 1;
+    }
+    if(jay_2_y < 0+radius){
+        directiony2 = 1;
+    }
+    jay_2_x = jay_2_x+2*directionx1;
+    jay_2_y = jay_2_y+2*directiony1;
 
 }
 
