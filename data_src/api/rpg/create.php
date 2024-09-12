bf1279951d473f3dc545fadcd320990f87231c77
Joey Wagner <joeyawagner22@gmail.com>
2024-09-12 08:41:39 -0400

    Adding location for RPG database changes

---
+++ data_src/api/rpg/create.php
@@ -0,0 +1,18 @@
+
+<?php
+require_once "../includes/db_config.php"; // Follow the lines
+
+// Create database connection
+$connection = new mysqli($host, $dbUsername, $dbPassword, $database);
+
+// Check if the connection was successful
+if ($connection->connect_error) {
+    die("Connection failed: ".$connection->connect_error);
+}
+
+// TODO: Get username, score, and maybe time?
+// Store in the database
+
+
+$connection->close();
+?>
