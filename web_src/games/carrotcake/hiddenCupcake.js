b266f3446bbf2617ccffd93474e985f558adf8ee
Laneyeh <humblel@etown.edu>
2024-09-12 09:57:56 -0400

    "added stub"

--- web_src/games/carrotcake/hiddenCupcake.js
+++ web_src/games/carrotcake/hiddenCupcake.js
@@ -1,7 +1,7 @@
 //Task Description: Place cupcakes hidden on the board and work to allow the actor to find and remove cupcakes and add to the score.
 const rows = board.length;
 const cols = board[0].length;
-
+let score = 0;
 let cupcakes = 0;
 // hiddenCupcake() function will allow me to place hidden cupcakes on the board for the user to collect:
 function hiddenCupcake(board, numCupcakes) {
@@ -22,8 +22,13 @@ function hiddenCupcake(board, numCupcakes) {
     return board;
 }
 //This function would be played IF the actor finds the cupcakes
-function findCupcake(){
-
+function findCupcake(board, playerRow, playerColumn){
+    //if the current position contains a cupcake(1), remove the cupcake from the board(0), update the score, and decrease the number of cupcakes hidden
+    if(board[playerRow][playerColumn] == 1){
+        score++;
+        cupcakes--;
+    } 
+    return board;
 }
 //This function would be played IF the actor removes the cupcakes
 function removeCupcake(){
