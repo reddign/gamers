06ef4661f7911728539978ac13c38bab9533af4e
Martin Ratchford <ratchfordm@etown.edu>
2024-09-12 12:29:41 -0400

    visit stats php and style sheet

---
+++ web_src/general/visitStats/visits.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+
+<html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visits Stats</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <h1>Visit Statistics</h1>
+    <table border="1">
+        <tr>
+            <th># Visited Once</th>
+            <th># Visited 2-5 Times</th>
+            <th># Visited 6+ Times</th>
+        </tr>
+        <tr>
+            <td>Loading...</td>
+            <td>Loading...</td>
+            <td>Loading...</td>
+        </tr>
+        <!-- Need to take data from a database and present it on this chart -->
+    </table>
+
+</body>
+</html>
