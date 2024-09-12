88b943a55817a5f311ee66bc6314ff41f176619a
STONERJ25 <stonerj@etown.edu>
2024-09-12 08:32:40 -0400

    Code bones

---
+++ web_src/games/JayHunt/jayhunt.js
@@ -0,0 +1,54 @@
+//Starting the intial Canvas
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+//Making our birds and setting them to be invisible
+let jay_1_x = 0;
+let jay_1_y = 0;
+let jay_2_x = 0;
+let jay_2_y = 0;
+let jay_1_see = 0;
+let jay_2_see = 0;
+let mouse_x = 0;
+let mouse_y = 0;
+
+let round = 0;
+
+function roundstart(){
+
+
+}
+
+function roundover(){
+
+
+
+}
+
+function fly(){
+    
+
+}
+
+function gameover(){
+
+
+}
+
+
+function net(e){
+
+}
+
+
+
+
+
+function animate(){
+    mouse_x = e.clientX;
+    mouse_y = e.clientY;
+
+}
+
+
+window.setInterval(animate,1000/FPS);
