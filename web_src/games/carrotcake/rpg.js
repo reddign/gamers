8d7dad3488a01b75b8c6a116afa7d605a72897e1
Joey Wagner <joeyawagner22@gmail.com>
2024-09-12 20:03:19 -0400

    Cleaning up stub code

--- web_src/games/carrotcake/rpg.js
+++ web_src/games/carrotcake/rpg.js
@@ -5,3 +5,31 @@ function animate(){
 function detectCOllision(){
     //add collision to the map
 }
+
+
+function reportScore() {
+    // TODO: report username, game, score, maybe time? to database
+    let gameStats = {
+        username: username,
+        gameName: gameName,
+        score: score,
+        // time: gameTime // maybe implemented depending on database capabilities
+    };
+
+    // TODO: Update below once more lectures have occurred 
+
+    // fetch('file where this will be stored.php', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(gameStats),
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log('Success', data);
+    // })
+    // .catch((error) => {
+    //     console.error('Error:', error);
+    // });
+}
