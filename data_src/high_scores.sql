03795cdd8054531d4257dbf23d952b15cbf62c29
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-09-15 14:23:35 -0400

    Update high_scores.sql

--- data_src/high_scores.sql
+++ data_src/high_scores.sql
@@ -2,18 +2,28 @@
 
 use triviagames;
 
--- 3. Create the highscores table
+-- TSK-8.1 Create a table to store the user, the date, the score, and the game played. (create table)
 CREATE TABLE IF NOT EXISTS highscores (
     user_id INT NOT NULL,                 	 -- Unique ID for each user (auto-incremented)
     game_played VARCHAR(100) NOT NULL,       -- Name of the game played
     score INT NOT NULL,                      -- Score achieved in the game
     time_played DATETIME NOT NULL,           -- Date the game was played
-    username VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,          -- Username for each user (picked)
     FOREIGN KEY (user_id) REFERENCES user(userID)
 );
 
--- 4. Insert sample data (optional)
-INSERT INTO highscores (user_id, game_played, score, date_played)
-VALUES 
-(100, 'Space Invaders', 1500, '2024-09-01'),
-(101, 'Pac-Man', 2000, '2024-09-02');
+-- TSK-8.2	Create a screen to show the top 10 players for each game.
+    -- high_scores.php which has a filepath of gamers/web_src/games/high_scores.php (R: Read for 8.3)
+
+-- TSK-8.3 Make sure that there are CRUD methods for this data in data_src/api/scoreRecord (possible folder name?)???
+    -- high_scores.sql add data to the table (C: Create for 8.3)
+    -- (CURRENTLY) Foreign Key Constraint not matched due to no current users
+INSERT INTO highscores(user_id, game_played, score, time_played, username)
+VALUES(1, 'Tetris', '1000000', NOW(), 'Dashing78');
+
+-- Notes:
+    -- 1) Have separate leaderboard page which can be filtered by game, player, date, etc
+    -- 2) Show leaderboard after each game, to show top 10 for the game just played
+	
+--  TSK-8.4	Update the documentation for these new CRUD methods.
+    -- Done in the high_scores.php file (R) & high_scores.sql file (C)
