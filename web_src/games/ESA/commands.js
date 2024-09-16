d85e3c0308b073eb56e42a642a49c001c7addd6a
mengle2332 <englep@etown.com>
2024-09-16 16:07:46 -0400

    Adding commands.js and example stub commands for later development

---
+++ web_src/games/ESA/commands.js
@@ -0,0 +1,43 @@
+
+// Initial test for text commands
+
+function Help(){
+    //Displays all commands
+
+}
+
+function Look(){
+    //Describes current surroundings
+
+}
+
+function Items(){
+    //Displays current inventory of held items
+
+}
+
+function Use(){
+    //Requires input after command (Use (insert item here)) to function
+    //Syntax example:   Use Key
+    //Interact with or use specified item
+
+}
+
+function Moveto(){
+    //Requires input after command (Moveto (location)) to function
+    //Syntax example:  Moveto BSC
+    //Moves player character to new location
+
+}
+
+function Grab(){
+    //Requires input after command (Grab (item)) to function
+    //Syntax example: Grab Key
+    //Grabs chosen item and places it into the players inventory
+
+}
+
+function Talk(){
+    //Interacts with / talks to nearby person (if available)    
+
+}
