5069f1a6c0e89ba64a784e4cd5c4f8a73582b3a5
Brian Duva <duvab@etown.edu>
2024-09-17 11:05:05 -0400

    adjusting page description

--- web_src/general/about/Brian_Duva.php
+++ web_src/general/about/Brian_Duva.php
@@ -17,7 +17,7 @@ require "../../includes/head.php";
         <section id="Jane Doe's Bio">
             <div class="bio-title">Brian Duva</div>
             <br>
-            <div id="bio"> 
+            <div id="bio"> Brian Duva is majoring in Computer Science with a concentration in Data Science, as well as a major in Math. He intends to graduate in 2027.
         </section>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ucv0b7iMMuVtWzmb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </main>
