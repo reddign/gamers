988251b09b4f8fadcfb93f32d316f6a669e9a3e4
Brian Duva <duvab@etown.edu>
2024-09-18 15:24:51 -0400

    initial code for squirrel movement

--- web_src/games/carrotcake/squirrel.js
+++ web_src/games/carrotcake/squirrel.js
@@ -10,8 +10,8 @@ class Squirrel {
         this.yPos = y;
 
         //target position of squirrel that it will path to
-        this.xTar = 0;
-        this.yTar = 0;
+        this.xTar = NaN;
+        this.yTar = NaN;
 
         //which targetting method the squirrel will use, will be varied to prevent all squirrels following the same path
         this.tarMethod = method;
@@ -23,12 +23,65 @@ class Squirrel {
         //called to update the squirrels target tile
         //method for changing the target tile depends on targetting method, similar to pac man ghosts
 
+        //update target tile if it is reached (will later be updated every movement tick instead)
+        if((this.xTar == this.xPos || isNaN(this.xTar)) && 
+           (this.yTar == this.yPos || isNaN(this.yTar))) {
+
+            //just pick a random tile for now
+            this.xTar = Math.floor(Math.random() * board.length);
+            this.yTar = Math.floor(Math.random() * board[this.xTar].length);
+
+        }
+
     }
 
-    move() {
+    //TODO: test the base movement function, add different ways of picking target tiles, and improve pathfinding
+    move(board) {
 
         //have the squirrel move towards their target tile
         //will have pathfinding code to help with naviagation and will hopefully have checks in place to prevent the squirrels from getting stuck
+
+        //initial pathfinding (0 is empty, 1 is cupcake, 2 is wall, 3 is player, 4 is a squirrel)
+        //this is not meant to stay or be definitive code, just temporary until further development is done
+
+        this.target(board);
+
+        board[this.xPos][this.yPos] = 0;
+
+        //if not aligned on x axis move on x, else move on y
+        if(this.xPos != this.xTar) {
+            
+            if(this.xPos > this.xTar) {
+
+                //check if position being moved to is a wall or cupcake and if not move
+                if(board[this.xPos - 1][this.yPos] != 1 && board[this.xPos - 1][this.yPos] != 2) this.xPos--;
+
+            } else {
+
+                //check if position being moved to is a wall or cupcake and if not move
+                if(board[this.xPos + 1][this.yPos] != 1 && board[this.xPos + 1][this.yPos] != 2) this.xPos++;
+
+            }
+
+        } else {
+
+            if(this.yPos > this.yTar) {
+
+                //check if position being moved to is a wall or cupcake and if not move
+                if(board[this.yPos - 1][this.yPos] != 1 && board[this.yPos - 1][this.yPos] != 2) this.yPos--;
+
+            } else {
+
+                //check if position being moved to is a wall or cupcake and if not move
+                if(board[this.yPos + 1][this.yPos] != 1 && board[this.yPos + 1][this.yPos] != 2) this.yPos++;
+
+            }
+
+        }
+
+        board[this.xPos][this.yPos] = 4;
+
+        return board;
 
     }
 
