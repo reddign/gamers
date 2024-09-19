45ef4e873e6dc7345416ae5bdab3460b64f85f8e
Owy137 <yango@etown.edu>
2024-09-19 09:05:43 -0400

    Dropdown menu for high_scores.php

--- web_src/games/high_scores.php
+++ web_src/games/high_scores.php
@@ -16,10 +16,12 @@ $result = $sql->get_result();
 
 //If there are results
 if($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      echo /* Add data */;
-    }
-  } 
+  echo "Select game";
+  foreach ($row = $r_set->fetch_assoc()) {
+  echo "<option value=$row[id]>$row[name]</option>";
+  }
+  echo "</select>";
+  }
   //If there has been no scores set
   else{
     echo "0 results";
@@ -27,38 +29,3 @@ if($result->num_rows > 0) {
   $conn->close();
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">    
-</head>
-<body>
-        <label>Select a Game</label>
-        <select name="Game">
-            <?php 
-                // use a while loop to fetch data 
-                // from the $all_categories variable 
-                // and individually display as an option
-                while ($category = mysqli_fetch_array(
-                        $all_categories,MYSQLI_ASSOC)):; 
-            ?>
-                <option value="<?php echo $category["Category_ID"];
-                    // The value we usually set is the primary key
-                ?>">
-                    <?php echo $category["Category_Name"];
-                        // To show the category name to the user
-                    ?>
-                </option>
-            <?php 
-                endwhile; 
-                // While loop must be terminated
-            ?>
-        </select>
-        <br>
-    <br>
-</body>
-</html>
