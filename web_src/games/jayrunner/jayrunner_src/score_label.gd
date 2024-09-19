e200e5cd89144fd9b87e1c008dfd5eef3f1de3d9
wheelsbot7 <o.wertzberger@gmail.com>
2024-09-19 09:36:33 -0400

    changed score display

--- web_src/games/jayrunner/jayrunner_src/score_label.gd
+++ web_src/games/jayrunner/jayrunner_src/score_label.gd
@@ -1,6 +1,6 @@
 extends Label
 
-var score = 0
+var score:int = 0
 var game_over = false
 var frames_since_last_increment = 0
 var frames_per_increment = 30  # Increment score every 30 frames
@@ -10,11 +10,8 @@ var frames_per_increment = 30  # Increment score every 30 frames
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if not game_over:
-		frames_since_last_increment += 1
-		if frames_since_last_increment >= frames_per_increment:
-			score += 1
-			frames_since_last_increment = 0
-		text = "Score: %d" % score
+		score += 1
+		text = "Score: %010d" % score
 
 func _on_obstacles_end_game() -> void:
 	game_over = true
