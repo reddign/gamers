016fa695066612fc4c828d5bad6cbb2a6e6c4910
Owen Yang <owy.yang@gmail.com>
2024-09-19 12:39:59 -0400

    Changes to high_scores.php

--- data_src/api/highscores/high_scores.php
+++ data_src/api/highscores/high_scores.php
@@ -3,7 +3,7 @@ require_once "../includes/db_config.php";
 session_start();
 
 // Create database connection
-$connection = new mysqli($host, $dbUsername, $dbPassword, $database);
+$connection = new mysqli("localhost", "root", "", "triviagames");
 
 if($connection->connect_error) {
     die("Connection failed: ".$connection->connect_error);
@@ -14,17 +14,18 @@ $sql = "SELECT DISTINCT game_played FROM highscores;";
 $result = $connection->query($sql);
 
 //If there are results
-if($result->num_rows > 0) {
-  echo "Select game";
-  foreach ($row = $r_set->fetch_assoc()) {
-  echo "<option value=$row[id]>$row[name]</option>";
+// if($result->num_rows > 0) {
+//   echo "<Select game>";
+   while($row = $result->fetch_assoc()) {
+//     echo "<option value='" . $row['game_played'] . "</option>";
+echo $row['game_played'];
   }
-  echo "</select>";
-  }
-  //If there has been no scores set
-  else{
-    echo "0 results";
-  }
+//   echo "</select>";
+//   }
+//   //If there has been no scores set
+//   else{
+//     echo "0 results";
+//   }
   $connection->close();
 
 ?>
