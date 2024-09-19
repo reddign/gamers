78dcb7b5b1c47db4649be08e12bb1c8faa9ff810
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-09-19 09:26:44 -0400

    highscores stuff update

--- data_src/api/highscores/high_scores.sql
+++ data_src/api/highscores/high_scores.sql
@@ -76,13 +76,23 @@ CREATE TABLE IF NOT EXISTS highscores (
 -- TSK-8.2	Create a screen to show the top 10 players for each game.
     -- high_scores.php which has a filepath of gamers/web_src/games/high_scores.php (R: Read for 8.3)
 
--- TSK-8.3 Make sure that there are CRUD methods for this data in data_src/api/scoreRecord (possible folder name?)???
-    -- high_scores.sql add data to the table (C: Create for 8.3)
-    -- (CURRENTLY) Foreign Key Constraint not matched due to no current users
+-- TSK-8.3 Make sure that there are CRUD methods for this data
+-- TSK-8.4	Update the documentation for these new CRUD methods.
+    -- Create (add data to the table) 
+        -- INSERT INTO highscores (user_id, game_played, score, time_played, username)
+        -- VALUES (user_id, game_played, score, time_played, username);
+
+    -- Read (read data from table)
+        -- Select username, game_played, score, time_played
+        -- From highscores
+        -- *** Filter by username, game_played, time_played ***
+        -- Always show by DESC
+         
+        -- Run Onto Highscores Page (whole table)
+        -- After game played, automatically filter by game_played (show top 10 DESC ... you) 
+        -- Be able to cgo to high scores page by click button from high scores showed after games
 
 -- Notes:
     -- 1) Have separate leaderboard page which can be filtered by game, player, date, etc
     -- 2) Show leaderboard after each game, to show top 10 for the game just played
-	
---  TSK-8.4	Update the documentation for these new CRUD methods.
-    -- Done in the high_scores.php file (R) & high_scores.sql file (C)
+    -- 3) (CURRENTLY) Foreign Key Constraint not matched due to no current users
