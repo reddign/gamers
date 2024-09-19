99ad01e6e094462a6d4c9076342b421e45411ca1
Joey Wagner <joeyawagner22@gmail.com>
2024-09-19 08:23:41 -0400

    Initial Creation of rpg.php

---
+++ web_src/games/carrotcake/rpg.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrot Cake Collection</title>
+    <script src="web_src\games\carrotcake\rpg.js"></script>
+</head>
+
+
+</html>
