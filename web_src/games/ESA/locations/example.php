172a2ebc5266ad4e925441a724bdd985ed8302b8
Austin Smith <smitha11@etown.edu>
2024-09-19 10:08:33 -0400

    First location set up

---
+++ web_src/games/ESA/locations/example.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>/*Title goes here*/</title>
+    <style>
+        body {
+            /* image directory: ".." in front takes it back a folder:
+            background-image: url('../pictures/{image name}');
+            background-size: cover;  /*This makes sure the image covers the entire page */
+            background-position: center 5%;
+            background-repeat: no-repeat; /* Ensures the image doesn’t repeat */
+        }
+    </style>
+</head>
+<body>
+    <h1>/*Caption shown on page</h1>
+</body>
+</html>
