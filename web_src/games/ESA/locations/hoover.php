ce5cae04a6d9d9e545386072c40b545e2f11f128
Mildred1999 <nwachukwu.mildred10@gmail.com>
2024-09-19 19:44:07 -0400

    adding background picture to some locations

---
+++ web_src/games/ESA/locations/hoover.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoover</title>
+    <style>
+        body {
+            background-image: url('../pictures/Hoover.png');
+            background-size: cover;  /*This makes sure the image covers the entire page */
+            background-position: center 5%;
+            background-repeat: no-repeat; /* Ensures the image doesn’t repeat */
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to Hoover!</h1>
+</body>
+</html>
