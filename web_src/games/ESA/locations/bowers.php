af6960197621490ab1d1a6f9a4202721a4a5d84a
Austin Smith <smitha11@etown.edu>
2024-09-19 10:04:21 -0400

    First location set up

--- web_src/games/ESA/locations/bowers.php
+++ web_src/games/ESA/locations/bowers.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Background Image</title>
+    <title>Bowers</title>
     <style>
         body {
-            background-image: url('../pictures/bowers.png');
-            /*background-size: cover;  This makes sure the image covers the entire page */
-            background-position: center;
+            background-image: url('../pictures/bowers.jpg');
+            background-size: cover;  /*This makes sure the image covers the entire page */
+            background-position: center 5%;
             background-repeat: no-repeat; /* Ensures the image doesn’t repeat */
         }
     </style>
 </head>
 <body>
-    <h1>Welcome to My PHP Page</h1>
+    <h1>Welcome to Bowers!</h1>
 </body>
 </html>
