1a34e7749cdaf24e4cb84f50c4a5bd1311021744
Joey Wagner <joeyawagner22@gmail.com>
2024-09-19 09:20:48 -0400

    Creating style.css and resources.js

--- web_src/games/carrotcake/carrotcake.php
+++ web_src/games/carrotcake/carrotcake.php
@@ -8,8 +8,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carrot Cake Collection</title>
+    <link rel="stylesheet" href="style.css">
     <script src="web_src\games\carrotcake\rpg.js"></script>
 </head>
-
+<body>
+    <canvas id="game-canvas" width="320" height="180"></canvas>
+</body>
 
 </html>
