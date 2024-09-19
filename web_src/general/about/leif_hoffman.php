3d923833f01a31449d2a2fb14e46abfdf684d550
Austin Smith <smitha11@etown.edu>
2024-09-19 12:28:07 -0400

    making leifs life hard

--- web_src/general/about/leif_hoffman.php
+++ web_src/general/about/leif_hoffman.php
@@ -13,7 +13,7 @@ require "../../includes/head.php";
         <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
     </div>
         #THIS IS A COMMENT 
-        # I like pizza.
+        # ASparagus ()*****Pancake
     <main>
         <section id="Leif Hoffman's Bio">
             <div class="bio-title">Leif Hoffman</div>
