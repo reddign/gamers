f169fa43c0ac9b052e6f810e8f585154d392406c
Camila Torres <torresc@etown.edu>
2024-09-19 10:50:55 -0400

    added graph and edited php

--- web_src/general/monthly_visits/monthlyvisits.php
+++ web_src/general/monthly_visits/monthlyvisits.php
@@ -16,6 +16,14 @@ session_start()
             <th>Month</th>
             <th>Number of Visitors</th>
         </tr>
+        <br>
+        <br>
+        <br>
+        <h2>Monthly Visits by Graph</h2>
+        <br>
+        <br>
+        <br>
+        <h2>Monthly Visits Table<h2>
 <?php
 // TODO: Connect to the database
 // example connection: $conn = new mysqli("localhost", "username", "password", "database");
@@ -44,3 +52,83 @@ session_start()
     </table>
 </body>
 </html
+
+<?php
+ 
+$dataPoints1 = array(
+	array("label"=> "2010", "y"=> 36.12),
+	array("label"=> "2011", "y"=> 34.87),
+	array("label"=> "2012", "y"=> 40.30),
+	array("label"=> "2013", "y"=> 35.30),
+	array("label"=> "2014", "y"=> 39.50),
+	array("label"=> "2015", "y"=> 50.82),
+	array("label"=> "2016", "y"=> 74.70)
+);
+$dataPoints2 = array(
+	array("label"=> "2010", "y"=> 64.61),
+	array("label"=> "2011", "y"=> 70.55),
+	array("label"=> "2012", "y"=> 72.50),
+	array("label"=> "2013", "y"=> 81.30),
+	array("label"=> "2014", "y"=> 63.60),
+	array("label"=> "2015", "y"=> 69.38),
+	array("label"=> "2016", "y"=> 98.70)
+);
+	
+?>
+<!DOCTYPE HTML>
+<html>
+<head>  
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Monthly Visits by Etown User"
+	},
+	axisY:{
+		includeZero: true
+	},
+	legend:{
+		cursor: "pointer",
+		verticalAlign: "center",
+		horizontalAlign: "right",
+		itemclick: toggleDataSeries
+	},
+	data: [{
+		type: "column",
+		name: "Etown User",
+		indexLabel: "{y}",
+		yValueFormatString: "$#0.##",
+		showInLegend: true,
+		dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+	},{
+		type: "column",
+		name: "Non-Etown User",
+		indexLabel: "{y}",
+		yValueFormatString: "$#0.##",
+		showInLegend: true,
+		dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+function toggleDataSeries(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+</body>
+</html>                 
