03e007e1e5c9f49fa555a41f3423e75b3a233cfd
SoudersT <tysouders32@gmail.com>
2024-09-20 18:25:55 -0400

    Trying to cHange again

--- web_src/general/about/Ian_Skeete.php
+++ web_src/general/about/Ian_Skeete.php
@@ -17,7 +17,13 @@ require "../../includes/head.php";
         <section id="Ian Skeete's Bio">
             <div class="bio-title">Ian Skeete</div>
             <br>
+<<<<<<< HEAD
             <div id="bio">"I am a freshamn information systems major with many minors. I will also be graduating this spring.
+=======
+            <div id="bio">"I am a freshamn information systems major with many minors. I will also be graduating this spring of 2065.
+            I am a an informs major with many minors
+            I am a man infos major with ma minors minors minors"
+>>>>>>> 3d58109 (Trying to cHange again)
         </section>
     </main>
 </body>
