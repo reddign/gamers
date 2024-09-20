89aef6607f38294f56b6f580dd7c37553f05fbce
SoudersT <tysouders32@gmail.com>
2024-09-20 17:37:14 -0400

    Revising Skeetes page

--- web_src/general/about/Ian_Skeete.php
+++ web_src/general/about/Ian_Skeete.php
@@ -17,7 +17,11 @@ require "../../includes/head.php";
         <section id="Ian Skeete's Bio">
             <div class="bio-title">Ian Skeete</div>
             <br>
-            <div id="bio">"I am a freshamn information systems major with many minors. I will also be graduating this spring of 2065."
+            <div id="bio">"I am a freshamn information systems major with many minors. I will also be graduating this spring of 2065.
+            I am a freshamn information systems major with many minors
+            I am a freshamn information systems major with many minors
+            I am a freshamn information systems major with many minors
+            I am a freshamn information systems major with many minors"
         </section>
     </main>
 </body>
