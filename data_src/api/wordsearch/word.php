3f3f5d8d8e394b20d59157f6256cdabb7f827caa
Asher Wayde <Waydea@etown.edu>
2024-09-23 10:36:28 -0400

    Finishing word.php

--- data_src/api/wordsearch/word.php
+++ data_src/api/wordsearch/word.php
@@ -2,12 +2,23 @@
 
 
 class Word{
+
+    /*
+    *The Word structure has 5 components
+    *Name is the string for the word
+    *Length is it's length
+    *Angle is the angle it will show up on the board
+    *Start is the start coords of the word
+    *End is the ending coords of the word
+    */
+
     public $name;
     public $length;
     public $angle=NULL;
     public $start=NULL;
     public $end=NULL;
 
+    // this sets up the structure with the name, and the length
     function __construct($name)
     {
         $this->name = $name;
@@ -15,9 +26,5 @@ class Word{
     }
 
 }
-// this is the basic word structure that goes on the board
-
-
-
 
 ?>
