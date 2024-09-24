3982d4170abbc057022f959b7e4cb798c23e3a76
James Hutchins <145177981+Jim-Hutchinson@users.noreply.github.com>
2024-09-24 10:50:54 -0400

    updated high_scores_functions

--- data_src/api/highscores/high_scores_functions.js
+++ data_src/api/highscores/high_scores_functions.js
@@ -1,7 +1,21 @@
 // Functions to interact with the high_scores database.
 
-// Function to insert data into the database
+```
+Using results return variable:
+
+    Results contains multiple different pieces of data about the SQL response.
+    results.xxxx will show that part of the response.
+```
+
 function insertScore(ID, game, score, date, username) {
+    ```
+    Function to insert scores into database.
+    ID: Unique ID for each user (auto-incremented)
+    game: Name of the game played
+    score: Score achieved in the game
+    date: Date the game was played
+    username: Username for each user (picked)
+    ```
     const query = 'INSERT INTO highscores (ID, game, score, date, username) VALUES (?, ?, ?, ?, ?)';
   
     connection.execute(query, [ID, game, score, date, username], (err, results) => {
@@ -9,5 +23,18 @@ function insertScore(ID, game, score, date, username) {
         console.error('Error inserting data:', err);
         return;
       }
+      return;
     });
+}
+
+
+function getTopScores(game){
+    ```
+    Function to get the top 10 scores from a game.
+    game: Name of the game played
+    ```
+
+    const query = 'SELECT top 10 * FROM highscores WHERE game_played = (?) ORDER BY highscore desc'
+    connection.execute(query, [game], (err, results))
+    return results;
 }
