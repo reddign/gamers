ce91b92e7c552943e16a10def929292bc1362842
Alex Roop <158513920+Acroop35@users.noreply.github.com>
2024-09-24 10:52:08 -0400

    added code to read from database

--- data_src/api/visit/read.php
+++ data_src/api/visit/read.php
@@ -1,9 +1,19 @@
 <?php
 require_once "../includes/db_connect.php"; // Follow the lines
 // Starts session
+$sql = "SELECT firstname, email, date FROM user WHERE date >= date_add(NOW(), INTERVAL -3 MONTH) ORDER BY userID DESC;";
 
+$result = $connection->query($sql);
+$users = array();
 
+if ($result->num_rows > 0) { 
+    while ($row = $result->fetch_assoc()) {
+        $users[] = $row["firstname"] . " " . $row["email"] . " " . $row["date"];
+    }
+}
 
+echo json_encode($users);
 
-
+// Close the database connection
+$connection->close();
 ?>
