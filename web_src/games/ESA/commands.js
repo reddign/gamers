3101a571dd774748aad1541159a1b27fd3954905
LeifHoffman <hoffmanl2@etown.edu>
2024-09-24 11:02:50 -0400

    Connected ESA php to commands js

--- web_src/games/ESA/commands.js
+++ web_src/games/ESA/commands.js
@@ -1,64 +1,63 @@
-// Initial test for text commands
+document.addEventListener('DOMContentLoaded', function () {
 
-var item = 'Key';       // This is testing, change later
-var location = 'esbenshade'; // This is testing, change later
+    // Initial test for text commands
+    const command = document.getElementById("Text_Input");
+    const button = document.getElementById("Button2")
 
-const command = document.getElementById("Text_Input");
-const button = document.getElementById("Button2")
+    button.addEventListener("click", function () {
+        if(command.value == "help"){
+            help();
+        }
+    });
 
-function Help(){
-    //Displays all commands
-    System.out.println("Help\n Look\n Items\n Use\n Moveto\n Grab\n Talk\n");
+    function help(){
+        //Displays all commands
+        window.alert("Help\n Look\n Items\n Use\n Moveto\n Grab\n Talk\n");
 
-}
+    }
 
-function Look(){
-    //Describes current surroundings
+    function look(){
+        //Describes current surroundings
 
-}
+    }
 
-function Items(){
-    //Displays current inventory of held items
+    function items(){
+        //Displays current inventory of held items
 
-    // const items = [""];
-    
+        // const items = [""];
+        
 
-    // for (i = 0; i < array.length; i++)
-    //     console.log(items);
+        // for (i = 0; i < array.length; i++)
+        //     console.log(items);
 
-}
+    }
 
-function Grab(){
-    //Requires input after command (Grab (item)) to function
-    //Syntax example: Grab Key
-    //Grabs chosen item and places it into the players inventory
+    function grab(){
+        //Requires input after command (Grab (item)) to function
+        //Syntax example: Grab Key
+        //Grabs chosen item and places it into the players inventory
 
-    items.push(item);
-}
+        items.push(item);
+    }
 
-function Use(){
-    //Requires input after command (Use (insert item here)) to function
-    //Syntax example:   Use Key
-    //Interact with or use specified item
+    function use(){
+        //Requires input after command (Use (insert item here)) to function
+        //Syntax example:   Use Key
+        //Interact with or use specified item
 
-}
+    }
 
-function Moveto(){
-    //Requires input after command (Moveto (location)) to function
-    //Syntax example:  Moveto BSC
-    //Moves player character to new location
+    function goto(){
+        //Requires input after command (Moveto (location)) to function
+        //Syntax example:  Moveto BSC
+        //Moves player character to new location
 
-}
+    }
 
 
-function Talk(){
-    //Interacts with / talks to nearby person (if available)    
+    function talk(){
+        //Interacts with / talks to nearby person (if available)    
 
-}
+    }
 
-// TESTING AREA BELOW, THIS WILL HAVE ALL OF THE LOCATIONS AND THE RELEVANT LOOK AND INTERACTION COMMANDS, THIS IS VERY SUBJECT TO CHANGE.
-
-const esbenshade = {
-    description: 'This is esbenshade'
-
-};
+});
