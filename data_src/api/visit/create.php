7988b2fd1aae24a628174ee7f311fbe330cdc89b
Alex Roop <158513920+Acroop35@users.noreply.github.com>
2024-09-24 10:29:58 -0400

    implementing db_connect file

--- data_src/api/visit/create.php
+++ data_src/api/visit/create.php
@@ -1,19 +1,6 @@
 <?php
-require_once "../includes/db_config.php"; // Follow the lines
-// Starts session
-session_start();
+require_once "../includes/db_connect.php"; // Follow the lines
 
-// Create database connection
-$connection = new mysqli($host, $dbUsername, $dbPassword, $database);
-
-// Check if the connection was successful
-if ($connection->connect_error) {
-    die("Connection failed: ".$connection->connect_error);
-}
-
-if (!isset($_POST['username'], $_POST['password'])) {
-    exit('Please complete the registration form!');
-}
 
 
 
--- data_src/api/visit/read.php
+++ data_src/api/visit/read.php
@@ -1,7 +1,6 @@
 <?php
 require_once "../includes/db_connect.php"; // Follow the lines
 // Starts session
-session_start();
 
 
 
