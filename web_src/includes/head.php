fe195991a9cb7040d30e2096efce1459e877e6be
Professor Reddig <reddign@etown.edu>
2024-09-24 13:32:20 -0400

    refactor navbar in register.

--- web_src/includes/head.php
+++ web_src/includes/head.php
@@ -9,7 +9,7 @@ session_start();
     <link rel="stylesheet" type="text/css" href="<?PHP echo url(); ?>/stylesheets/about.css">
 <?php
 //load extra css files if the page has an array of $cssFiles
-if(is_array($cssFiles) && count($cssFiles)>0 ){
+if(isset($cssFiles) && is_array($cssFiles) && count($cssFiles)>0 ){
     $url=url();
     foreach($cssFiles as $cssfile){
         echo '<link rel="stylesheet" type="text/css" href="'.$url.'/stylesheets/'.$cssfile.'">';
@@ -22,7 +22,7 @@ if(is_array($cssFiles) && count($cssFiles)>0 ){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php
 //load extra js files if the page has an array of $jsFiles
-if(is_array($jsFiles) && count($jsFiles)>0 ){
+if(isset($jsFiles) && is_array($jsFiles) && count($jsFiles)>0 ){
     $url=url();
     foreach($jsFiles as $jsfile){
         echo "<script src='".$jsfile."'></script>";
