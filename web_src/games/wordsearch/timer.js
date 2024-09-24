9bb3508d498b03c6e408f00d22043722e34679cb
Asher Wayde <ashbwayde@gmail.com>
2024-09-24 14:41:05 -0400

    Making the timer functional

--- web_src/games/wordsearch/timer.js
+++ web_src/games/wordsearch/timer.js
@@ -1,9 +1,27 @@
 
+let interval=null;
+window.addEventListener('load',init);
 
-//window.addEventListener('load',init);
+async function init(){
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    await delay(5000);
+    interval=window.setInterval(updateTimer,1000);
+}
 
-let canvas=document.getElementById("timer");
-let draw=canvas.getContext("2d");
-draw.font="30px Arial";
-draw.fillText("12:00");
-console.log(draw);
+function updateTimer(){
+    let timer=document.getElementById("timer");
+    let time=timer.innerHTML;
+    let timeArr=time.split(":");
+    let mins=parseInt(timeArr[0]);
+    let secs=parseInt(timeArr[1]);
+    let totalSecs=secs+(mins*60);
+    if(totalSecs<=1)
+        window.clearInterval(interval);
+    totalSecs-=1;
+    if(totalSecs%60<10){
+        timer.innerHTML=(Math.floor(totalSecs/60))+":0"+(totalSecs%60);
+    }
+    else{
+        timer.innerHTML=(Math.floor(totalSecs/60))+":"+(totalSecs%60);
+    }
+}
