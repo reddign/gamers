ac743513fbe30968f8cec25b4fcd567e2778db82
Asher Wayde <ashbwayde@gmail.com>
2024-09-24 10:30:52 -0400

    Adding basic timer file

---
+++ web_src/games/wordsearch/timer.js
@@ -0,0 +1,9 @@
+
+
+//window.addEventListener('load',init);
+
+let canvas=document.getElementById("timer");
+let draw=canvas.getContext("2d");
+draw.font="30px Arial";
+draw.fillText("12:00");
+console.log(draw);
