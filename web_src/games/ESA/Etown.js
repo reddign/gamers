4e6f5ae83be19867afd4f93098fa01fbeeaf3b28
Mildred1999 <nwachukwu.mildred10@gmail.com>
2024-09-25 00:11:22 -0400

    created a function to send scores to database

--- web_src/games/ESA/Etown.js
+++ web_src/games/ESA/Etown.js
@@ -1,5 +1,5 @@
 let score = 0;
-let name;
+let username;
 let major;
 
 
@@ -29,4 +29,28 @@ function has_keycard(){
 
 function first_lecture(){
     
+}
+
+// This function sends scores to the database created by the other group
+function sendScores(){
+    fetch(// This should be the file path for the data file,
+         {
+        method: "POST",
+        body: JSON.stringify({
+            user_name: username,
+            game_played: "Etown Student Adventure",
+            score: score
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+function gameOver(){
+    // set criteria for game over
+    sendScores() // send scores after game over
+
 }
