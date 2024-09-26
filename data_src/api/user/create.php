7bb7343a4d3bb25ec631ee563e5ff70bcbf52fcf
Alex Roop <158513920+Acroop35@users.noreply.github.com>
2024-09-26 08:48:46 -0400

    adding variable to check for session variables

--- data_src/api/user/create.php
+++ data_src/api/user/create.php
@@ -13,6 +13,8 @@ $username = $_POST["user"]; // Receive the username
 $email = $_POST["email"]; // Get email
 $firstname = $_POST["first"];
 
+$initialname = isset($_SESSION['username']) ? $_SESSION['username'] :'';  
+
 // We're doing it the hard way I guess
 $sql = $connection->prepare("INSERT INTO user (username, email, firstname, date) VALUES (?, ?, ?, DATE_FORMAT(NOW(), '%Y-%m-%d'));");
 $sql->bind_param("sss", $username, $email, $firstname); // No SQLi allowed
