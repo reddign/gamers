faa4b5a8ddb501cefcabcce7d2f54f6767f373f9
wheelsbot7 <o.wertzberger@gmail.com>
2024-09-26 09:15:17 -0400

    added speed increase over time

--- web_src/games/jayrunner/jayrunner_src/obstacle.gd
+++ web_src/games/jayrunner/jayrunner_src/obstacle.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 var game_over = false
-@export var SCROLL_SPEED = 300
+var SCROLL_SPEED:int
 
 func _ready():
 	position.x = 1400
-	$Enemy.play(var_to_str(randi_range(1, 3)))
+	$Enemy.play(var_to_str(randi_range(1, 3)), SCROLL_SPEED / 300)
 	add_to_group("obstacles")
+	add_to_group("scrollers")
 
 func _process(delta):
 	if not game_over:
@@ -24,3 +25,6 @@ func _on_body_entered(_body: CharacterBody2D) -> void:
 func stop_moving():
 	game_over = true
 	$Enemy.pause()
+
+func update_scrolling_speed(CURRENT_SPEED):
+	SCROLL_SPEED = CURRENT_SPEED
