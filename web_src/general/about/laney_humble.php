2aab37f95467c3ba6ea0331d8a16d6b89a89c1c2
Laneyeh <humblel@etown.edu>
2024-09-26 23:53:38 -0400

    made changes to bio page

--- web_src/general/about/laney_humble.php
+++ web_src/general/about/laney_humble.php
@@ -18,24 +18,8 @@ require "../../includes/head.php";
         <section id="Laney's Bio">
             <div class="bio-title">Laney Humble</div>
             <br>
-            <div id="bio"> Laney Humble is a Junior at Elizabethtown College. She participates in Music on campus.
-                           She is a Graphic Design and 
-                           Computer Science (Concentration in Web and Application Design) dual major who is 
-                           planning on graduating in May, 2026.
-
-<<<<<<< HEAD
-                           Laney Humble is an amazing, incredible person.
-=======
-                           Laney Humblean incredible person.
->>>>>>> ba1e89b (added to bio page)
-                           She is thoughtful, kind, and caring.
-                           She loves to hang out with friends and get to know other people.
-                           She is going to make an amazing web developer in the future.
-                           
-                           Computer Science(Concentration in Web and Application Design) dual major who is planning to graduate 
-                           in May, 2026.
-
-                           Laney is in Melica Acapella, WWEC Radio Station, and SWE.
+            <div id="bio"> Laney Humble is a Junior at Elizabethtown College. She is a Graphic Design and Computer Science 
+                            (Concentration in Web and Application Design) dual major who is planning on graduating in May, 2026.
         </section>
     </main>
 </body>
