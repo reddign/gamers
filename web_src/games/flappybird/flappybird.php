d4f994890e1aa0b50138b57b387692eb6e72d4ad
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-09-29 21:57:27 -0400

    scoring test w flappy bird
    Testing
    1) create.php
    2) flappybird.js
    3) flappybird.php
    4) high_scores_functions.js

--- web_src/games/flappybird/flappybird.php
+++ web_src/games/flappybird/flappybird.php
@@ -9,7 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flappy Bird Game</title>
     <link rel="stylesheet" href="../../stylesheets/flappybird.css">
-    <link rel="stylesheet" href="../../stylesheets/pacman.css">
+    <link rel="stylesheet" href="../../stylesheets/hangman.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
@@ -81,8 +81,8 @@ session_start();
         <div class="pipe" id="pipeBottom2"></div>
         
     </div>
-    <script src="flappybird.js"></script>
+    <script type="module" src="../high_scores_functions.js"></script>
+    <script type="module" src="flappybird.js"></script>
     </main>
 </body>
 </html>
-
