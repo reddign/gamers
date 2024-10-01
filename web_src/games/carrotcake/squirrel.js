5968eb259792dfcaa0da9becc343a3a821b3ceec
Brian Duva <duvab@etown.edu>
2024-10-01 05:25:32 -0400

    stub code for differest squirrel movement patterns

--- web_src/games/carrotcake/squirrel.js
+++ web_src/games/carrotcake/squirrel.js
@@ -22,19 +22,94 @@ class Squirrel {
 
     }
 
-    target() {
+    target(board) {
 
         //called to update the squirrels target tile
         //method for changing the target tile depends on targetting method, similar to pac man ghosts
 
         //update target tile if it is reached (will later be updated every movement tick instead)
         //stub code for different methods
-        if(self.tarMethod == "direct") {
 
-        } else if(self.tarMethod == "flank") {
+        //these values would have to be grabbed in some way
+        let playerX = 0;
+        let playerY = 0;
+        let playerDir = "left";
+        
+        let player_dist = 35; //placeholder numbers (calculate later), squirrels will not pathfind directly to player when past a certain distance
+        let dist_thresh = 30; //helps with reducing difficulty since otherwise squirrels will always be able to follow and corner the player
 
-        } else if(self.tarMethod == "intercept") {
+        if(player_dist < dist_thresh) {
+        
+            if(self.tarMethod == "direct") {
+
+                //set target coordinates to player coordinates
+                self.xTar = playerX;
+                self.yTar = playerY;
+
+            } else if(self.tarMethod == "flank") {
+
+                //set target coordinates to just behind the player
+                switch(playerDir) {
+                    case "left":
+                            self.xTar = playerX + 2;
+                            self.yTar = playerY;
+                        break;
+
+                    case "up":
+                            self.xTar = playerX;
+                            self.yTar = playerY + 2;
+                        break;
+
+                    case "right":
+                            self.xTar = playerX - 2;
+                            self.yTar = playerY;
+                        break;
+
+                    case "down":
+                            self.xTar = playerX;
+                            self.yTar = playerY - 2;
+                        break;
+
+                    default:
+                            self.xTar = playerX;
+                            self.yTar = playerY;
+                        break;
+
+                }
+
+            } else if(self.tarMethod == "intercept") {
             
+                //set target coordinates to just in front of player
+                switch(playerDir) {
+                    case "left":
+                            self.xTar = playerX - 2;
+                            self.yTar = playerY;
+                        break;
+
+                    case "up":
+                            self.xTar = playerX;
+                            self.yTar = playerY - 2;
+                        break;
+
+                    case "right":
+                            self.xTar = playerX + 2;
+                            self.yTar = playerY;
+                        break;
+
+                    case "down":
+                            self.xTar = playerX;
+                            self.yTar = playerY + 2;
+                        break;
+
+                    default:
+                            self.xTar = playerX;
+                            self.yTar = playerY;
+                        break;
+
+                }
+
+            }
+        
         } else {
 
             if((this.xTar == this.xPos || isNaN(this.xTar)) && 
@@ -45,6 +120,16 @@ class Squirrel {
                 this.yTar = Math.floor(Math.random() * board[this.xTar].length);
 
             }
+
+        }
+
+        if(self.xTar < 0 || self.xTar >= board.length) {
+
+            self.xTar = self.xTar % board.length;
+
+        } else if(self.yTar < 0 || self.yTar >= board[self.xTar].length) {
+
+            self.yTar = self.xTar % board[self.xTar].length;
 
         }
 
