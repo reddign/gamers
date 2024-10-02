1ed1ca6571b3e57330fafa358e40e520fad345bd
mengle2332 <englep@etown.com>
2024-10-01 21:06:27 -0400

    Adding 'hello'

--- web_src/general/about/ParkerEngle.php
+++ web_src/general/about/ParkerEngle.php
@@ -20,7 +20,7 @@ require "../../includes/head.php";
             <div id="bio"> Parker Engle is a student in this class. They are majoring in Computer Science,
                 and intends to graduate in 2027.
 
-                prof reddig here.  Hello
+                prof reddig here.  Hello There!
         </section>
     </main>
 </body>
