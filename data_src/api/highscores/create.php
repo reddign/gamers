10128121024b18e8d4f455a185de8ad595b3161a
James Hutchins <145177981+Jim-Hutchinson@users.noreply.github.com>
2024-10-03 20:46:15 -0400

    rework create.php and temp.js

--- data_src/api/highscores/create.php
+++ data_src/api/highscores/create.php
@@ -1,18 +1,25 @@
 <?php
-require_once "../includes/db_config.php";
+require "../includes/db_config.php";
 
 session_start();
 
 echo '<script src="./temp.js"></script>';
 
-require "../includes/db_config.php";
 $information = file_get_contents('php://input');
 error_log($information);
 $data = json_decode($information);
+
 //Get the posted data
-$score = $data->score;
-$game = $data->game;
-$username = $data->user;
+$ID = isset($_POST['userID']) ? $_POST['userID'] : '';
+
+$game = isset($_POST['game']) ? $_POST['game'] : '';
+
+//if(isset($_POST['score'])) $score = $_POST['score'];
+$score = isset($_POST['score']) ? $_POST['score'] : '';
+
+$timeplayed = isset($_POST['timeplayed']) ? $_POST['timeplayed'] : '';
+
+$username = isset($_POST['username']) ? $_POST['username'] : '';
 
 //Connect to a Database
 $mysqli = new mysqli($host,$dbUsername,$dbPassword,$database);
@@ -22,10 +29,10 @@ if ($mysqli->connect_errno) {
 
 //Prepare and execute the SQL statement
 $stmt = $mysqli->prepare("INSERT INTO highscores
-(username,game_played,score,time_played)
+(user_id,game_played,score,time_played,username)
 VALUES
-(?,?,?,NOW())");
-$stmt->bind_param("sss",$username, $game, $score);
+(?,?,?,?,?)");
+$stmt->bind_param("isiss",$ID,$game, $score, $timeplayed, $username);
 $stmt->execute();
 $stmt->close();
 $mysqli->close();
