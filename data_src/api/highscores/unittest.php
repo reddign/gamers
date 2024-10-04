ebdeb606921397968eba32b0743ff620c8da3b2e
James Hutchins <145177981+Jim-Hutchinson@users.noreply.github.com>
2024-10-04 11:52:44 -0400

    sendscores and create now work

---
+++ data_src/api/highscores/unittest.php
@@ -0,0 +1,13 @@
+<h1>Testing the API in create.php file</h1>
+
+<form method="Post" action="create.php">
+User ID:<input type="text" name='userID'><BR>
+Game:<input type="text" name='game'><BR>
+Date:<input type="text" name='timeplayed'><BR>
+Score:<input type="text" name='score'><BR>
+User Name:<input type="text" name='username'><BR>
+<input type="submit" value='Unit Test'><BR>
+</form>
+
+<script src="temp.js"></script>
+<button onclick="saveScores();">Test the JS</button>
