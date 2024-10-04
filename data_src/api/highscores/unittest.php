d4ee8a74d838d210ad0096a2c23f6dee5838e8fb
James Hutchins <145177981+Jim-Hutchinson@users.noreply.github.com>
2024-10-04 16:00:20 -0400

    trying stuff I guess

--- data_src/api/highscores/unittest.php
+++ data_src/api/highscores/unittest.php
@@ -1,13 +1,16 @@
 <h1>Testing the API in create.php file</h1>
 
 <form method="Post" action="create.php">
-User ID:<input type="text" name='userID'><BR>
-Game:<input type="text" name='game'><BR>
-Date:<input type="text" name='timeplayed'><BR>
-Score:<input type="text" name='score'><BR>
-User Name:<input type="text" name='username'><BR>
-<input type="submit" value='Unit Test'><BR>
+    User ID:<input type="text" name='userID'><BR>
+    Game:<input type="text" name='game'><BR>
+    Date:<input type="text" name='timeplayed'><BR>
+    Score:<input type="text" name='score'><BR>
+    User Name:<input type="text" name='username'><BR>
+
+    <input type="submit" value='Unit Test'><BR>
 </form>
 
-<script src="temp.js"></script>
-<button onclick="saveScores();">Test the JS</button>
+<h1>Test that JS sends to API</h1>
+<script src="scoreSend.js"></script>
+<script src="unittest.js"></script>
+<button onclick="runButtonTest();">Test the JS</button>
