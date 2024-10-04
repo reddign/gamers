35cf3adf92b3a83c707dabe1cd28be47098c2501
James Hutchins <145177981+Jim-Hutchinson@users.noreply.github.com>
2024-10-03 20:46:15 -0400

    rework create.php and temp.js

--- data_src/api/highscores/temp.js
+++ data_src/api/highscores/temp.js
@@ -2,8 +2,10 @@ function sendScores(){
     fetch("./create.php", {
         method: "POST",
         body: JSON.stringify({
+            user_id: userid,
             game: gameName,
             score: score,
+            timeplayed: time,
             user: userName
         }),
         headers: {
@@ -15,8 +17,9 @@ function sendScores(){
         .catch(error => console.error('Error: ',error));
 }
 
+let userid = 1;
 let gameName = "test";
 let score = 666;
-let user = "Dummy0"
-
+let time = Date.now();
+let user = "Dummy0";
 sendScores();
