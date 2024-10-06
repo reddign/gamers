56c24e58d72f5c51609bbcfc36d233c17109fce8
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-10-06 02:07:01 -0400

    Massive Update
    1) Added comments and documentation to the code for highscores
    2) scoreSend.js, create.php, and unittest.php were changed to allow calling from the old php way or now the new js way (opens the capabilities for that in the future)
    3) Deleted temp.js & unittest.js as scoreSend.js has the basic ideas of these two files

--- data_src/api/highscores/scoreSend.js
+++ data_src/api/highscores/scoreSend.js
@@ -1,22 +1,31 @@
+/* Sample formats of inputs
+    let userID = 1;
+    let gameName = "test";
+    let score = 666;
+    let time = '2024-10-04 12:35:45';
+    let user = "";
+*/
 
-
-
-function sendScores(userid,game,score,time,username){
-    fetch("./create.php", {
-        method: "POST",
-        body: JSON.stringify({
-            user_id: userid,
-            game: game,
-            score: score,
-            timeplayed: time,
-            user: username
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
-        }
-      })
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch(error => console.error('Error: ',error));
+function sendScores(userID, game, score, timeplayed, username) {
+  fetch("./create.php", {
+      method: "POST",
+      body: JSON.stringify({
+          user_id: userID,
+          game_played: game,
+          score: score,
+          time_played: timeplayed,
+          username: username
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+  })
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then((json) => console.log(json))
+  .catch(error => console.error('Error: ', error));
 }
-
