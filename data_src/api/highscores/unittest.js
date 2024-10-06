61e416efb0b6edc3d838ab66b6914599e4b10b6e
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-10-06 02:07:01 -0400

    Massive Update
    1) Added comments and documentation to the code for highscores
    2) scoreSend.js, create.php, and unittest.php were changed to allow calling from the old php way or now the new js way (opens the capabilities for that in the future)
    3) Deleted temp.js & unittest.js as scoreSend.js has the basic ideas of these two files

--- data_src/api/highscores/unittest.js
+++
@@ -1,9 +0,0 @@
-ID = 1;
-game = "test";
-date = "2024-10-04 10:31:18";
-score = 111;
-username = "tester1";
-
-function runButtonTest(){
-    sendScores(ID, game, date, score, username);
-}
--- data_src/api/highscores/unittest.php
+++ data_src/api/highscores/unittest.php
@@ -1,16 +1,25 @@
 <h1>Testing the API in create.php file</h1>
 
-<form method="Post" action="create.php">
-    User ID:<input type="text" name='userID'><BR>
-    Game:<input type="text" name='game'><BR>
-    Date:<input type="text" name='timeplayed'><BR>
-    Score:<input type="text" name='score'><BR>
-    User Name:<input type="text" name='username'><BR>
+<form method="POST" action="create.php">
+    User ID: <input type="text" name='userID' required><br>
+    Game: <input type="text" name='game' required><br>
+    Score: <input type="text" name='score' required><br>
+    Date: <input type="text" name='timeplayed' required><br>
+    User Name: <input type="text" name='username' required><br>
 
-    <input type="submit" value='Unit Test'><BR>
+    <input type="submit" value='Unit Test'><br>
 </form>
 
+<!-- 
+    Takes the inputs of the form above,
+    passes it into sendScores to show example of using JS function
+-->
 <h1>Test that JS sends to API</h1>
 <script src="scoreSend.js"></script>
-<script src="unittest.js"></script>
-<button onclick="runButtonTest();">Test the JS</button>
+<button onclick="sendScores(
+    document.querySelector('input[name=\'userID\']').value,
+    document.querySelector('input[name=\'game\']').value,
+    document.querySelector('input[name=\'score\']').value,
+    document.querySelector('input[name=\'timeplayed\']').value,
+    document.querySelector('input[name=\'username\']').value
+);">Test the JS</button>
