271bdce5a0e78fa2ea54240ddc26c2a3163c10f2
LeifHoffman <hoffmanl2@etown.edu>
2024-10-08 10:09:51 -0400

    Moved functions from Etown js to commands js, removed Etown js

--- web_src/games/ESA/commands.js
+++ web_src/games/ESA/commands.js
@@ -81,4 +81,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 
+    // This function sends scores to the database created by the other group
+    function sendScores(){
+        fetch(// This should be the file path for the data file,
+            {
+            method: "POST",
+            body: JSON.stringify({
+                user_name: username,
+                game_played: "Etown Student Adventure",
+                score: score
+            }),
+            headers: {
+            "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
+
+    function gameOver(){
+        // set criteria for game over
+        sendScores() // send scores after game over
+
+    }
+
 });
