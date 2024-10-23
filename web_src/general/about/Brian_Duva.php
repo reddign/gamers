7248df66a1e75b96344b91a64799d0f5fa7d903b
Owy137 <yango@etown.edu>
2024-10-22 20:24:14 -0400

    Update Brian_Duva.php

--- web_src/general/about/Brian_Duva.php
+++ web_src/general/about/Brian_Duva.php
@@ -2,7 +2,6 @@
 $pageName = "Brian Duva's Webpage";
 require "../../includes/functions.php";
 require "../../includes/head.php";
-
 ?>
 <body>
 <?php
@@ -12,13 +11,14 @@ require "../../includes/head.php";
     <div class="buttons">
         <a href="SEteam2024.php"><button class="button button2">Go Back!</button></a> 
     </div>
-        
+
     <main>
         <section id="Jane Doe's Bio">
-            <div class="bio-title">Jane Doe</div>
+            <div class="bio-title">Brian Duva</div>
             <br>
             <div id="bio"> Brian Duva is majoring in Computer Science with a concentration in Data Science, as well as a major in Math. He intends to graduate in 2027.
         </section>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ucv0b7iMMuVtWzmb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </main>
 </body>
 <?php
